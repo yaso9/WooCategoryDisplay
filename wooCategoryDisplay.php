78c07300a58e6dcf123ab6b925b242dd6a082ca2
yaso9 <yahyasohail@hotmail.com>
2016-12-21 12:36:56 -0600

    Editted Padding

--- wooCategoryDisplay.php
+++ wooCategoryDisplay.php
@@ -21,7 +21,7 @@ function displayProductCategories() {
 
 	echo '<div class="categoryDisplay" style="padding-top: 40px;">';
 	foreach ($categories as $category) {
-			echo '<div style="display:inline-block;padding-right:10px; width:250px;vertical-align:top;" class="categoryDisplayItem ' . ($category->parent != 0 ? 'child ' . $category->parent : '') . '"><center><img style="display:block;width:250px;height:250px!important;object-fit:cover;" src="' . (getCategoryThumbnail($category->term_taxonomy_id) ? getCategoryThumbnail($category->term_taxonomy_id) : plugins_url() . '/wooCategoryDisplay/noImageAvailable.png') . '"/></center>';
+			echo '<div style="display:inline-block;padding-right:10px;width:225px;vertical-align:top;padding-bottom:20px;" class="categoryDisplayItem ' . ($category->parent != 0 ? 'child ' . $category->parent : '') . '"><center><img style="display:block;width:225px;height:225px!important;object-fit:cover;" src="' . (getCategoryThumbnail($category->term_taxonomy_id) ? getCategoryThumbnail($category->term_taxonomy_id) : plugins_url() . '/wooCategoryDisplay/noImageAvailable.png') . '"/></center>';
 			echo '<span class="info" style="display:none;">' . isParent($categories, $category->term_taxonomy_id) . ', ' . $category->term_taxonomy_id . '</span>';
 			echo '<center><span style="display:none;" class="idOfCategory">' . $category->term_taxonomy_id . '</span><a href="/index.php/product-category/' . $category->slug . '"><h3>' . $category->name . '</h3></a></center></div>';
 	}
