ed7376614fe5d2036de6bfbcebe7584059d37972
BuildTools <unconfigured@null.spigotmc.org>
2016-12-20 19:42:54 -0600

    init

---
+++ wooCategoryDisplay.js
@@ -0,0 +1,26 @@
+(function ($) {
+	$(document).ready(function () {
+		$('.child').hide()
+
+		$('.categoryDisplay').find('.categoryDisplayItem').each(function (index) {
+			let info = $(this).find('.info').html().split(',')
+			let isParent = info[0];
+
+			if (!isParent) {
+				$(this).on('click', function () {
+					window.location.assign($(this).find('a').attr('href'))
+			})
+			}
+
+			if (isParent) {
+				$(this).find('a').on('click', function (event) {
+					event.preventDefault()
+				})
+				$(this).on('click', function (event) {
+					$(this).parents('.categoryDisplay').find('.categoryDisplayItem').not('child').hide()
+					$(this).parents('.categoryDisplay').find('.categoryDisplayItem.child.' + info[1].trim()).show()
+				})
+			}
+		})
+	})
+})(jQuery)
---
+++ wooCategoryDisplay.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Plugin Name: Woo Category Display
+ * Plugin URI: http://captaincybertech.com
+ * Description: This plugin adds shortcode for category display
+ * Version: 1.0.0
+ * Author: Captain Cyber
+ * Author URI: http://captaincybertech.com
+ * License: MIT
+ */
+
+wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.1.1.min.js');
+wp_enqueue_script('wooCategoryDisplay', plugins_url() . '/wooCategoryDisplay/wooCategoryDisplay.js', array('jquery'));
+
+function displayProductCategories() {
+	$categories = get_terms('product_cat');
+
+	// echo "<pre>";
+	// var_dump($categories);
+	// echo "</pre>";
+
+	echo '<div class="categoryDisplay"><center>';
+	foreach ($categories as $category) {
+			echo '<div style="display:inline-block; padding-right: 10px; max-width:250px; vertical-align:top;" class="categoryDisplayItem ' . ($category->parent != 0 ? 'child ' . $category->parent : '') . '"><center><img style="display:block;width:250px;" src="' . (getCategoryThumbnail($category->term_taxonomy_id) ? getCategoryThumbnail($category->term_taxonomy_id) : plugins_url() . '/wooCategoryDisplay/noImageAvailable.png') . '"/></center>';
+			echo '<span class="info" style="display:none;">' . isParent($categories, $category->term_taxonomy_id) . ', ' . $category->term_taxonomy_id . '</span>';
+			echo '<center><span style="display:none;" class="idOfCategory">' . $category->term_taxonomy_id . '</span><a href="/index.php/product-category/' . $category->slug . '"><h3>' . $category->name . '</h3></a></center></div>';
+	}
+	echo '</center></div>';
+}
+
+function isParent($categories, $id) {
+	foreach ($categories as $category) {
+		if ($category->parent == $id) {
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+
+function getCategoryThumbnail($categoryId) {
+	$thumbnail_id = get_woocommerce_term_meta( $categoryId, 'thumbnail_id', true );
+	return wp_get_attachment_url($thumbnail_id);
+}
+
+add_shortcode('displayProductCategories', 'displayProductCategories');
