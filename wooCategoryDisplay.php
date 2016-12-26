f9732462d7a259e0526cfc7989e149011d9421ea
yaso9 <yahyasohail@hotmail.com>
2016-12-26 13:53:38 -0600

    basic back button

--- wooCategoryDisplay.php
+++ wooCategoryDisplay.php
@@ -23,7 +23,11 @@ function displayProductCategories() {
 	foreach ($categories as $category) {
 			echo '<div style="display:inline-block;padding-right:10px;width:225px;vertical-align:top;padding-bottom:20px;" class="categoryDisplayItem ' . ($category->parent != 0 ? 'child ' . $category->parent : '') . '"><center><img style="display:block;width:225px;height:225px!important;object-fit:cover;" src="' . (getCategoryThumbnail($category->term_taxonomy_id) ? getCategoryThumbnail($category->term_taxonomy_id) : plugins_url() . '/wooCategoryDisplay/noImageAvailable.png') . '"/></center>';
 			echo '<span class="info" style="display:none;">' . isParent($categories, $category->term_taxonomy_id) . ', ' . $category->term_taxonomy_id . '</span>';
-			echo '<center><span style="display:none;" class="idOfCategory">' . $category->term_taxonomy_id . '</span><a href="/index.php/product-category/' . parentSlug($categories, $category->term_taxonomy_id) . '/' . $category->slug . '"><h3>' . $category->name . '</h3></a></center></div>';
+			echo '<center><a href="/index.php/product-category/' . parentSlug($categories, $category->term_taxonomy_id) . '/' . $category->slug . '"><h3>' . $category->name . '</h3></a></center></div>';
+      if (isParent($categories, $Category->term_taxonomy_id)) {
+        echo '<div style="display:inline-block;padding-right:10px;width:225px;vertical-align:top;padding-bottom:20px;" class="back categoryDisplayItem ' . 'child ' . $category->term_taxonomy_id . '"><center><img style="display:block;width:225px;height:225px!important;object-fit:cover;" src="' . (plugins_url() . '/wooCategoryDisplay/backButton.png') . '"/></center>';
+        echo '<center><a href="' . $_SERVER['REQUEST_URI'] . '"><h3>Go Back</h3></a></center></div>';
+      }
 	}
 	echo '</div>';
 }
